bd7dcbf125848f9edcb02f897b9f41a013150f85
megbellam <58276976+megbellam@users.noreply.github.com>
2024-04-19 13:23:47 -0700

    Update asg5b.js

--- asg5b.js
+++ asg5b.js
@@ -126,33 +126,7 @@ function main() {
 
 	];
 
-	{
-		//Function to position the camera at a distance where a model fits on the screen
-		function frameArea( sizeToFitOnScreen, boxSize, boxCenter, camera ) {
-
-			const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
-			const halfFovY = THREE.MathUtils.degToRad( camera.fov * .5 );
-			const distance = halfSizeToFitOnScreen / Math.tan( halfFovY );
-			// compute a unit vector that points in the direction the camera is now
-			// from the center of the box
-			const direction = ( new THREE.Vector3() ).subVectors( camera.position, boxCenter ).normalize();
 	
-			// move the camera to a position distance units way from the center
-			// in whatever direction the camera was from the center already
-			camera.position.copy( direction.multiplyScalar( distance ).add( boxCenter ) );
-	
-			// pick some near and far values for the frustum that
-			// will contain the box.
-			camera.near = boxSize / 100;
-			camera.far = boxSize * 100;
-	
-			camera.updateProjectionMatrix();
-	
-			// point the camera to look at the center of the box
-			camera.lookAt( boxCenter.x, boxCenter.y, boxCenter.z );
-	
-		}
-
 		const objLoader = new OBJLoader();
 		const mtlLoader = new MTLLoader();
   		mtlLoader.load('https://megbellam.github.io/test/necklace.mtl', (mtl) => {
